ffee7c8e58ecd34bbaa7297b44a4c4285c74926c
KUNDAN KUMAR <133587375+kundan4317@users.noreply.github.com>
2024-10-03 17:52:53 +0530

    Create compressor.java

---
+++ compressor.java
@@ -0,0 +1,38 @@
+package comp_decomp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ *
+ * @author kundan kumar raghav
+ */
+public class compressor {
+    public static void method(File file)throws IOException{
+        String fileDirectory=file.getParent();
+        FileInputStream fis=new FileInputStream(file);
+        FileOutputStream fos=new FileOutputStream(fileDirectory+"/compressedFile.gz");
+        GZIPOutputStream gzip=new GZIPOutputStream(fos);
+        
+        byte[] buffer=new byte[1024];
+        int len;
+        
+        while((len=fis.read(buffer))!=-1){
+            gzip.write(buffer, 0, len);
+            
+        }
+        gzip.close();
+        fos.close();
+        fis.close();
+        
+        
+    }
+    public static void main(String[] args)throws IOException{
+        File path=new File("C:/Users/kundan kumar raghav/Downloads/ResearchPaper.docx");
+        method(path);
+    }
+    
+}
